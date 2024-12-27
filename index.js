dc772c9c94834e8789e27d69a1cfe81b33904d6c
Avanish Vadke <140254982+AvanishVadke@users.noreply.github.com>
2024-12-27 11:49:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ app.use(express.json());
 app.use(cors());
 
 const con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Avanish@100',
-    database: 'sms26dec24'
+    host: "sql12.freesqldatabase.com", // Corrected: no trailing spaces
+    user: "sql12754019",
+    password: "nMIUkfg9X9",
+    database: "sql12754019"
 });
 
 app.post("/ss", (req, res) => {
-    let sql = "insert into student value(?,?,?)";
+    let sql = "insert into student2 value(?,?,?)";
     let data = [req.body.rno, req.body.name, req.body.marks];
     con.query(sql, data, (err, result) => {
         if (err) res.send(err);
@@ -23,7 +23,7 @@ app.post("/ss", (req, res) => {
 });
 
 app.get("/gs", (req, res) => {
-    let sql = "select * from student";
+    let sql = "select * from student2";
     con.query(sql, (err, result) => {
         if (err) res.send(err);
         else res.send(result);
@@ -31,7 +31,7 @@ app.get("/gs", (req, res) => {
 });
 
 app.put("/us", (req, res) => {
-    let sql = "update student set name = ?, marks = ? where rno = ?";
+    let sql = "update student2 set name = ?, marks = ? where rno = ?";
     let data = [req.body.name, req.body.marks, req.body.rno];
     con.query(sql, data, (err, result) => {
         if (err) res.send(err);
@@ -40,7 +40,7 @@ app.put("/us", (req, res) => {
 });
 
 app.delete("/ds/:rno", (req, res) => {
-    let sql = "delete from student where rno = ?";
+    let sql = "delete from student2 where rno = ?";
     let data = [req.params.rno]; // Retrieve rno from the URL
     con.query(sql, data, (err, result) => {
         if (err) 
